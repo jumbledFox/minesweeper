3353481125c8007341e978fa088263a7ec7077a0
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2024-03-10 22:58:43 +0000

    ...
    i am ill

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,18 +3,47 @@ pub mod button;
 pub mod menu_bar;
 pub mod dropdown;
 pub mod popup;
-use ggez::glam::Vec2;
+use ggez::{glam::Vec2, graphics::Rect};
 pub use text_renderer::TextRenderer;
 pub use button::Button;
 pub use menu_bar::MenuBar;
 pub use dropdown::Dropdown;
 pub use popup::Popup;
 
+#[derive(Debug)]
+enum DropdownGroupType {
+    None, PickOne,
+}
+
+const MENU_OPTIONS: &[(&str, &[(DropdownGroupType, &[&str])])] = &[
+    ("Game", &[
+        (DropdownGroupType::None,    &["New Game"]),
+        (DropdownGroupType::PickOne, &["Easy    10*10, 9", "Normal 15*13,40", "Hard   30*16,99", "Custom..."]),
+        // (DropdownGroupType::ToggleEach, &["Question marks"]), // Maybe another day, who needs question marks?
+        (DropdownGroupType::None,    &["Exit"]),
+    ]),
+    ("Scale", &[
+        (DropdownGroupType::PickOne, &[" 1x ", " 2x ", " 3x ", " 4x ", " 5x ", " 6x ", " 7x ", " 8x "]),
+    ]),
+    ("Help", &[
+        (DropdownGroupType::None, &["How to play", "About"]),
+    ]),
+];
+
+enum DropdownGroupInfo {
+    None, PickOne{selected: usize},
+}
+
 #[derive(PartialEq, Clone, Copy)]
 pub enum MousePressMode {
     None, Down, Up,
 }
 pub struct Gui {
+    // menubar: [Button; 3],
+
+    // The menu bar button, a vector of each button in the dropdown
+    menubar: [(Button, Vec<DropdownGroupInfo>, Vec<(Button, usize)>); MENU_OPTIONS.len()],
+
     pub menu_bar: MenuBar,
     pub popup: Option<Popup>,
     hovered_on_gui: bool,
@@ -24,7 +53,12 @@ pub struct Gui {
 
 impl Gui {
     pub fn new(menu_bar: MenuBar) -> Gui {
-        Gui { menu_bar, hovered_on_gui: false, popup: None }
+        println!("{:?}", MENU_OPTIONS);
+
+        let menubar: [(Button, Vec<DropdownGroupInfo>, Vec<Button>); MENU_OPTIONS.len()];
+        
+        todo!();
+        // Gui { menu_bar, hovered_on_gui: false, popup: None }
     }
     pub fn update(&mut self, mouse_pos: Vec2, mouse_mode: MousePressMode) {
         self.menu_bar.update(mouse_pos, mouse_mode);
