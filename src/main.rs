35a9e7e1721d607465091e4d0eeac8a1b7dc24ed
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2024-04-05 14:19:22 +0100

    do some work on menubars

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use macroquad::{miniquad::window::request_quit, prelude::*};
-use ui::UIState;
+use macroquad::{miniquad::window::{order_quit, request_quit}, prelude::*};
+use ui::{ButtonState, UIState};
 
 pub mod ui;
 pub mod minesweeper;
@@ -14,17 +14,33 @@ async fn main() {
 
         ui.prepare();
         
+        ui.begin_menubar();
+        if ui.menu_item(String::from("Game")) {
+            // if ui.dropdown("Exit") { order_quit(); }
+        }
+        if ui.menu_item(String::from("Help")) {
+            // if ui.dropdown("About") {  }
+        }
+        if ui.menu_item(String::from("Scale")) {
+            // if ui.dropdown("Exit") { order_quit(); }
+        }
+        
         ui.checkbox(0, &mut c, 10.0, 10.0, 20.0, 20.0);
         if c {
-            if ui.button(1, 10.0, 40.0, 50.0, 20.0) {
+            if ui.button(1, 10.0, 40.0, 50.0, 20.0) == ButtonState::Released {
                 println!("hello");
             }
+            for i in 0..10 {
+                if ui.button(i+10, 130.0, i as f32*25.0 + 40.0, 80.0, 20.0) == ButtonState::Released {
+                    println!("{:?}", i);
+                }
+            }
         }
-        if ui.button(2, 10.0, 80.0, 50.0, 20.0) {
+        if ui.button(2, 10.0, 80.0, 50.0, 20.0) == ButtonState::Released {
             println!("hello 2");
         }
-        if ui.button(3, 40.0, 80.0, 70.0, 40.0) {
-            request_quit();
+        if ui.button(3, 40.0, 80.0, 70.0, 40.0) == ButtonState::Released {
+            order_quit();
         }
 
         ui.finish();
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,6 +2,7 @@ use macroquad::prelude::*;
 use macroquad::math::Vec2;
 
 pub enum DrawShape {
+    Label{x: f32, y: f32, text: String, color: Color},
     Rect{x: f32, y: f32, w: f32, h: f32, color: Color},
 }
 
@@ -27,6 +28,8 @@ pub struct UIState {
     hot_item: SelectedItem,
     active_item: SelectedItem,
 
+    menubar_info: MenubarInfo,
+
     drawqueue: Vec<DrawShape>,
 }
 
@@ -37,6 +40,8 @@ impl UIState {
             mouse_down: false,
             hot_item: SelectedItem::None,
             active_item: SelectedItem::None,
+            
+            menubar_info: MenubarInfo::new(),
 
             drawqueue: vec![],
         }
@@ -60,8 +65,9 @@ impl UIState {
 
         // Draw all of the elements so the first ones are drawn last and appear on top
         for d in self.drawqueue.iter().rev() {
-            match *d {
-                DrawShape::Rect { x, y, w, h, color } => draw_rectangle(x, y, w, h, color), 
+            match d {
+                DrawShape::Label { x, y, text, color } => draw_text(&text, *x, *y, 40.0, *color),
+                &DrawShape::Rect { x, y, w, h, color } => draw_rectangle(x, y, w, h, color), 
             }
         }
     }
@@ -73,39 +79,84 @@ impl UIState {
         self.mouse_pos.y <  y + h
     }
 
+    pub fn label(&mut self, text: String, color: Color, x: f32, y: f32) {
+        self.drawqueue.push(DrawShape::Label { x, y, text, color });
+    }
+}
 
-    pub fn button(&mut self, id: usize, x: f32, y: f32, w: f32, h: f32) -> bool { 
+#[derive(PartialEq)]
+pub enum ButtonState {
+    Clicked, Released, Held, Hovered, Idle,
+}
+
+impl UIState {
+    pub fn button(&mut self, id: usize, x: f32, y: f32, w: f32, h: f32) -> ButtonState {
+        let mut clicked = false;
         // Detect whether the button should be hot / active
         if self.hot_item == SelectedItem::None && self.mouse_in_rect(x, y, w, h) {
             self.hot_item = SelectedItem::Some(id);
             if self.active_item == SelectedItem::None && self.mouse_down {
                 self.active_item = SelectedItem::Some(id);
+                clicked = true;
             }
         }
 
         // Rendering
-        let (rect_x, rect_y, color) = match (&self.hot_item, &self.active_item) {
-            // Hot and active (Pressed)
-            (hot_item, active_item) if *hot_item == id && *active_item == id => (x + 2.0, y + 2.0, Color::from_hex(0xAAAAAA)),
+        let (rect_x, rect_y, color, state) = match (&self.hot_item, &self.active_item) {
+            // Hot and active (Held)
+            (hot_item, active_item) if *hot_item == id && *active_item == id => (x + 2.0, y + 2.0, Color::from_hex(0xAAAAAA), ButtonState::Held),
             // Hot, but not active (Hovered)
-            (hot_item, _) if *hot_item == id => (x, y, Color::from_hex(0xFFFFFF)),
+            (hot_item, _) if *hot_item == id => (x, y, Color::from_hex(0xFFFFFF), ButtonState::Hovered),
             // Otherwise
-            _  => (x, y, Color::from_hex(0xDDDDDD)),
+            _  => (x, y, Color::from_hex(0xDDDDDD), ButtonState::Idle),
         };
         self.drawqueue.push(DrawShape::Rect { x: rect_x, y: rect_y, w, h, color });
         // Draw the shadow
         self.drawqueue.push(DrawShape::Rect { x: x + 4.0, y: y + 4.0, w, h, color: Color::from_hex(0x000000) });
 
-        // If the button is hot and active, but the mouse isn't down, the user must've clicked the button
-        !self.mouse_down && self.hot_item == id && self.active_item == id
+        // If the button is hot and active, but the mouse isn't down, the user must've released the button
+        if !self.mouse_down && self.hot_item == id && self.active_item == id {
+            return ButtonState::Released;
+        } else {
+            if clicked {
+                return ButtonState::Clicked;
+            } else {
+                return state;
+            }
+        }
     }
 
     pub fn checkbox(&mut self, id: usize, value: &mut bool, x: f32, y: f32, w: f32, h: f32) {
+        let drawqueue_before_index = self.drawqueue.len();
+
+        let offset = match self.button(id, x, y, w, h) {
+            ButtonState::Held => 2.0,
+            ButtonState::Released => {*value = !*value; 0.0}
+            _ => 0.0
+        };
         if *value {
-            self.drawqueue.push(DrawShape::Rect { x: x+w/4.0, y: y+h/4.0, w: w*(2.0/4.0), h: h*(2.0/4.0), color: Color::from_hex(0x333333) });
+            self.drawqueue.insert(drawqueue_before_index, DrawShape::Rect { x: x+w/4.0+offset, y: y+h/4.0+offset, w: w*(2.0/4.0), h: h*(2.0/4.0), color: Color::from_hex(0x333333) });
         }
-        if self.button(id, x, y, w, h) {
-            *value = !*value;
-        }
+    }
+}
+
+struct MenubarInfo {
+    along: f32,
+}
+impl MenubarInfo {
+    pub fn new() -> MenubarInfo {
+        MenubarInfo { along: 0.0 }
+    }
+}
+
+impl UIState {
+    pub fn begin_menubar(&mut self) {
+
+    }
+    pub fn menu_item(&mut self, text: String) -> bool {
+        
+        self.label(text, Color::from_hex(0xFF0000), 10.0, 30.0);
+        
+        true
     }
 }
