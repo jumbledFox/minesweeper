84aeca7a04734be7ddc385db36b8ab71717f45f1
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2024-04-07 23:58:29 +0100

    good progress!!
    im making things less autogenerated and more hard coded but way easier for me.
    no layoutbuilder or any wacky stuff like that for THIS fox!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use macroquad::{miniquad::window::{order_quit, request_quit}, prelude::*};
+use macroquad::{miniquad::window::order_quit, prelude::*};
 use ui::{ButtonState, UIState};
 
 pub mod ui;
@@ -12,34 +12,66 @@ async fn main() {
     loop {
         clear_background(Color::from_hex(0x756853));
 
-        ui.prepare();
+        ui.begin();
         
         ui.begin_menubar();
-        if ui.menu_item(String::from("Game")) {
-            // if ui.dropdown("Exit") { order_quit(); }
+        if ui.menu_item(String::from("Game"), 50.0) {
+            if ui.dropdown(String::from("New Game"))   {  }
+            ui.dropdown_separator();
+            if ui.dropdown(String::from("Easy"))   {  }
+            if ui.dropdown(String::from("Normal")) {  }
+            if ui.dropdown(String::from("Hard"))   {  }
+            if ui.dropdown(String::from("Custom")) {  }
+            ui.dropdown_separator();
+            if ui.dropdown(String::from("Exit")) { order_quit(); }
         }
-        if ui.menu_item(String::from("Help")) {
-            // if ui.dropdown("About") {  }
+        if ui.menu_item(String::from("Help"), 25.0) {
+            if ui.dropdown(String::from("About")) {  }
         }
-        if ui.menu_item(String::from("Scale")) {
-            // if ui.dropdown("Exit") { order_quit(); }
+        if ui.menu_item(String::from("Scale"), 15.0) {
+            if ui.dropdown(String::from(" 1* ")) {}
+            if ui.dropdown(String::from(" 2* ")) {}
+            if ui.dropdown(String::from(" 3* ")) {}
+            if ui.dropdown(String::from(" 4* ")) {}
+            ui.dropdown_new_column();
+            if ui.dropdown(String::from(" 5* ")) {}
+            if ui.dropdown(String::from(" 6* ")) {}
+            if ui.dropdown(String::from(" 7* ")) {}
+            if ui.dropdown(String::from(" 8* ")) {}
         }
+        ui.finish_menubar();
         
-        ui.checkbox(0, &mut c, 10.0, 10.0, 20.0, 20.0);
-        if c {
-            if ui.button(1, 10.0, 40.0, 50.0, 20.0) == ButtonState::Released {
-                println!("hello");
-            }
-            for i in 0..10 {
-                if ui.button(i+10, 130.0, i as f32*25.0 + 40.0, 80.0, 20.0) == ButtonState::Released {
-                    println!("{:?}", i);
-                }
-            }
-        }
-        if ui.button(2, 10.0, 80.0, 50.0, 20.0) == ButtonState::Released {
+        // ui.checkbox(String::from("cb1"), &mut c, 10.0, 10.0, 20.0, 20.0);
+        // if c {
+        //     ui.label(
+        //         String::from("Hello!! this is a test of the ui label..\nthat should've been a line break.. :3"),
+        //         Color::from_hex(0x00FFFF), 250.0, 40.0
+        //     );
+        //     ui.label(
+        //         String::from("Yippee!! It seems to be working. [] {} () - + = * /"),
+        //         Color::from_hex(0xFF3333), 250.0, 60.0
+        //     );
+        //     ui.label(
+        //         String::from("This invalid character, which isn't in the CHAR_MAP, \nshould be shown as a question mark. -> 🦊 <-. It's actually a fox emoji!"),
+        //         Color::from_hex(0xFFFF00), 250.0, 70.0
+        //     );
+        //     ui.label(
+        //         String::from("jumbledFox"),
+        //         Color::from_hex(0x000000), 250.0, 90.0
+        //     );
+        //     if ui.button(1, 10.0, 40.0, 50.0, 20.0) == ButtonState::Released {
+        //         println!("hello");
+        //     }
+        //     for i in 0..10 {
+        //         if ui.button(i+10, 130.0, i as f32*25.0 + 40.0, 80.0, 20.0) == ButtonState::Released {
+        //             println!("{:?}", i);
+        //         }
+        //     }
+        // }
+        if ui.button(String::from("Hello!"), 10.0, 80.0, 50.0, 20.0) == ButtonState::Released {
             println!("hello 2");
         }
-        if ui.button(3, 40.0, 80.0, 70.0, 40.0) == ButtonState::Released {
+        if ui.button(String::from("Quit"), 40.0, 80.0, 70.0, 40.0) == ButtonState::Released {
             order_quit();
         }
 
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,24 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+// TODO:
+// Make it so that there's a LayoutBuilder that aligns all of the elements to the right position.
+// This will mean a frame of lag.. but ¯\_(ツ)_/¯
+// I'd also like to have a stack of Windows, which are just an ID and a rect. It could be reordered which would be good,
+// I can use the last frames window stack to see if I should interact with a window / elements or not
+// Maybe I could give everything a stack.. idk
+
+pub fn hash_string(input: &String) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    input.hash(&mut hasher);
+    hasher.finish()
+}
+
 use macroquad::prelude::*;
 use macroquad::math::Vec2;
+
+use self::text_renderer::TextRenderer;
+
+pub mod text_renderer;
 
 pub enum DrawShape {
     Label{x: f32, y: f32, text: String, color: Color},
@@ -9,12 +28,12 @@ pub enum DrawShape {
 #[derive(PartialEq)]
 enum SelectedItem {
     None,
-    Some(usize),
+    Some(u64),
     Unavailable,
 }
 
-impl PartialEq<usize> for SelectedItem {
-    fn eq(&self, other: &usize) -> bool {
+impl PartialEq<u64> for SelectedItem {
+    fn eq(&self, other: &u64) -> bool {
         match &self {
             SelectedItem::Some(s) if *s == *other => true,
             _ => false,
@@ -30,6 +49,7 @@ pub struct UIState {
 
     menubar_info: MenubarInfo,
 
+    text_renderer: TextRenderer,
     drawqueue: Vec<DrawShape>,
 }
 
@@ -40,17 +60,19 @@ impl UIState {
             mouse_down: false,
             hot_item: SelectedItem::None,
             active_item: SelectedItem::None,
-            
+
             menubar_info: MenubarInfo::new(),
 
+            text_renderer: TextRenderer::new(),
             drawqueue: vec![],
         }
     }
 
-    pub fn prepare(&mut self) {
+    pub fn begin(&mut self) {
         self.mouse_pos = Vec2::new(mouse_position().0, mouse_position().1);
         self.mouse_down =  is_mouse_button_down(MouseButton::Left);
         self.hot_item = SelectedItem::None;
+        self.menubar_info.reset();
         self.drawqueue = Vec::new();
     }
     
@@ -66,7 +88,7 @@ impl UIState {
         // Draw all of the elements so the first ones are drawn last and appear on top
         for d in self.drawqueue.iter().rev() {
             match d {
-                DrawShape::Label { x, y, text, color } => draw_text(&text, *x, *y, 40.0, *color),
+                DrawShape::Label { x, y, text, color } => self.text_renderer.draw_text(text, *x, *y, *color, None),
                 &DrawShape::Rect { x, y, w, h, color } => draw_rectangle(x, y, w, h, color), 
             }
         }
@@ -90,7 +112,9 @@ pub enum ButtonState {
 }
 
 impl UIState {
-    pub fn button(&mut self, id: usize, x: f32, y: f32, w: f32, h: f32) -> ButtonState {
+    pub fn button(&mut self, text: String, x: f32, y: f32, w: f32, h: f32) -> ButtonState {
+        let id = hash_string(&text);
+
         let mut clicked = false;
         // Detect whether the button should be hot / active
         if self.hot_item == SelectedItem::None && self.mouse_in_rect(x, y, w, h) {
@@ -110,9 +134,12 @@ impl UIState {
             // Otherwise
             _  => (x, y, Color::from_hex(0xDDDDDD), ButtonState::Idle),
         };
+        let label_rel_pos = (Vec2::new(w, h)-self.text_renderer.text_size(&text, None))/2.0;
+
+        self.drawqueue.push(DrawShape::Label { x: rect_x + label_rel_pos.x, y: rect_y + label_rel_pos.y, text, color: Color::from_hex(0x000000) });
         self.drawqueue.push(DrawShape::Rect { x: rect_x, y: rect_y, w, h, color });
         // Draw the shadow
-        self.drawqueue.push(DrawShape::Rect { x: x + 4.0, y: y + 4.0, w, h, color: Color::from_hex(0x000000) });
+        // self.drawqueue.push(DrawShape::Rect { x: x + 4.0, y: y + 4.0, w, h, color: Color::from_hex(0x000000) });
 
         // If the button is hot and active, but the mouse isn't down, the user must've released the button
         if !self.mouse_down && self.hot_item == id && self.active_item == id {
@@ -126,10 +153,10 @@ impl UIState {
         }
     }
 
-    pub fn checkbox(&mut self, id: usize, value: &mut bool, x: f32, y: f32, w: f32, h: f32) {
+    pub fn checkbox(&mut self, label: String, value: &mut bool, x: f32, y: f32, w: f32, h: f32) {
         let drawqueue_before_index = self.drawqueue.len();
 
-        let offset = match self.button(id, x, y, w, h) {
+        let offset = match self.button(label, x, y, w, h) {
             ButtonState::Held => 2.0,
             ButtonState::Released => {*value = !*value; 0.0}
             _ => 0.0
@@ -141,11 +168,27 @@ impl UIState {
 }
 
 struct MenubarInfo {
-    along: f32,
+    height: f32,
+    current_pos: f32,
+    next_pos: f32,
+    dropdown_width: f32,
+    dropdown_x: f32,
+    dropdown_y: f32,
 }
+
 impl MenubarInfo {
     pub fn new() -> MenubarInfo {
-        MenubarInfo { along: 0.0 }
+        MenubarInfo {
+            height: 0.0,
+            current_pos: 0.0,
+            next_pos: 0.0,
+            dropdown_width: 0.0,
+            dropdown_x: 1.0,
+            dropdown_y: 1.0,
+        }
+    }
+    pub fn reset(&mut self) {
+        self.next_pos = 0.0;
     }
 }
 
@@ -153,10 +196,58 @@ impl UIState {
     pub fn begin_menubar(&mut self) {
 
     }
-    pub fn menu_item(&mut self, text: String) -> bool {
+
+    pub fn finish_menubar(&mut self) {
+        self.drawqueue.push(DrawShape::Rect {
+            x: 0.0,
+            y: 0.0,
+            w: 9999.0,
+            h: self.menubar_info.height,
+            color: Color::from_hex(0xC0CBDC) })
+    }
+
+    pub fn menu_item(&mut self, text: String, dropdown_width: f32) -> bool {
+        self.menubar_info.dropdown_width = dropdown_width;
+        self.menubar_info.dropdown_x = 1.0;
+        self.menubar_info.dropdown_y = 1.0;
+
+        self.menubar_info.current_pos = self.menubar_info.next_pos;
         
-        self.label(text, Color::from_hex(0xFF0000), 10.0, 30.0);
+        let size = self.text_renderer.text_size(&text, None) + Vec2::new(4.0, 2.0);
         
+        let b = self.button(text.clone(), self.menubar_info.current_pos, 0.0, size.x, size.y);
+
+        self.menubar_info.next_pos += size.x;
+        self.menubar_info.height = size.y;
+
         true
+    }
+
+    // TODO: Align all buttons text to the left
+    pub fn dropdown(&mut self, text: String) -> bool {
+        let size = self.text_renderer.text_size(&text, None) + Vec2::new(4.0, 2.0);
+        
+        let b = self.button(
+            text.clone(),
+            self.menubar_info.dropdown_x + self.menubar_info.current_pos,
+            self.menubar_info.dropdown_y + self.menubar_info.height,
+            self.menubar_info.dropdown_width, 
+            size.y
+        );
+        self.menubar_info.dropdown_y += size.y;
+
+        b == ButtonState::Released
+    }
+    // TODO: Add dropdown_radio and dropdown_checkbox types
+    pub fn dropdown_radio<T>(&mut self, value: &mut T, assign: &mut T) {
+        
+    }
+
+    pub fn dropdown_separator(&mut self) {
+        self.menubar_info.dropdown_y += 4.0 
+    }
+    pub fn dropdown_new_column(&mut self) {
+        self.menubar_info.dropdown_x += self.menubar_info.dropdown_width;
+        self.menubar_info.dropdown_y = 1.0;
     }
 }
